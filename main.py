b3c3f0602dc0a45ec254106a1e1b8329379e9e55
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-03-22 15:53:39 -0300

    Update
    Implementando melhorias no algoritmo de busca, foi adicionado mais um algoritmo (de ordenação) conhecido como merge sort. Se optar pelo uso, basta tirar os comentários, pois já está pronto para funcionamento, e também foi implementado uma função de formatação para os cpfs, e por fim, foi otimizar algumas partes do código.

--- main.py
+++ main.py
@@ -1,5 +1,54 @@
 import csv
+import re
 
+# Implementando algoritmo de oerdenação merge sort
+# def executar_merge_sort(lista, inicio=0, fim=None):
+#     if not fim:
+#         fim = len(lista)
+
+#     if fim - inicio > 1:
+#         meio = (inicio + fim) // 2
+#         executar_merge_sort(lista, inicio, meio)
+#         executar_merge_sort(lista, meio, fim)
+#         executar_merge(lista, inicio, meio, fim)
+
+#     return lista
+
+# def executar_merge(lista, inicio, meio, fim):
+#     esquerda = lista[inicio:meio]
+#     direita = lista[meio:fim]
+#     topo_esquerda = topo_direita = 0
+
+#     for p in range(inicio, fim):
+#         if topo_esquerda >= len(esquerda):
+#             lista[p] = direita[topo_direita]
+#             topo_direita += 1
+#         elif topo_direita >= len(direita):
+#             lista[p] = esquerda[topo_esquerda]
+#             topo_esquerda += 1
+#         elif esquerda[topo_esquerda] < direita[topo_direita]:
+#             lista[p] = esquerda[topo_esquerda]
+#             topo_esquerda += 1
+#         else:
+#             lista[p] = direita[topo_direita]
+#             topo_direita += 1
+
+# # Implementando algoritmo de busca binária
+# def executar_busca_binaria(lista, valor):
+#     minimo = 0
+#     maximo = len(lista) - 1
+
+#     while minimo <= maximo:
+#         meio = (minimo + maximo) // 2
+#         if valor < lista[meio]:
+#             maximo = meio - 1
+#         elif valor > lista[meio]:
+#             minimo = meio + 1
+#         else:
+#             return True
+
+#     return False
+         
 # Impementando algorítmo de busca linear
 def executar_busca_linear(lista, valor):
     tamanho_lista = len(lista)
@@ -10,20 +59,18 @@ def executar_busca_linear(lista, valor):
 
 # Função que faz dedup e tratamento no cpf
 def criar_lista_dedup(lista):
-    lista_dedup = []
+    lista_dedup = set()
     for cpf in lista:
         cpf = str(cpf)
         cpf = cpf.replace("-", "").replace(".", "")
         if len(cpf) == 11:
-            if not executar_busca_linear(lista_dedup, cpf):
-                lista_dedup.append(cpf) 
+            lista_dedup.add(cpf)
 
-    return lista_dedup
+    return list(lista_dedup)
 
 # Função para tratar os CPFs
 def tratar_cpf(cpf):
-    cpf_tratado = cpf.replace(".", "").replace("-", "")
-    return cpf_tratado
+    return re.sub(r"[.-]", "", cpf)
 
 # Função para ler o arquivo csv
 def ler_cpf_arquivo(nome_arquivo):
@@ -31,15 +78,29 @@ def ler_cpf_arquivo(nome_arquivo):
     with open(nome_arquivo, mode='r', newline='') as arquivo_csv:
         leitor_csv = csv.reader(arquivo_csv)
         for linha in leitor_csv:
-            cpfs.append(tratar_cpf(linha[0]))
-        
+            if linha:
+                cpfs.append(tratar_cpf(linha[0]))
+
     return cpfs
+
+# Função para formatar CPF
+def formatar_cpf(cpf):
+    cpf = re.sub(r"[.-]", "", cpf)
+    return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
 
 # Função de teste
 def testar_funcao(nome_arquivo):
     lista_cpfs = ler_cpf_arquivo(nome_arquivo)
     lista_dedup = criar_lista_dedup(lista_cpfs)
+    
+    cpfs_formatados = [formatar_cpf(cpf) for cpf in lista_dedup]
+    cpfs_formatados.sort(key=None)
+    
+    print(f'Quantidade de CPFs deduplicados: {len(lista_dedup)}\n')
+    print('CPFs corrigidos e tratados:')
     print(lista_dedup)
+    print('\nCPFs formatados:')
+    print(cpfs_formatados)
 
 nome_arquivo = "cpfs.csv"
 testar_funcao(nome_arquivo)
